9ca2cd0f243096bca3e74c0b1012f30e7d833dca
Rounak <rounak.29.agarwal@gmail.com>
2022-01-13 00:50:27 +0530

    port

--- app.js
+++ app.js
@@ -150,12 +150,7 @@ app.get("/:customListName",function(req,res){
     
 })
 
-let port = process.env.PORT;
-if(port == null || port == ""){
-    port=3000;
-}
-app.listen(port);
 
-app.listen(port,function(){
+app.listen(process.env.PORT || 3000,function(){
     console.log("server running");
 })
